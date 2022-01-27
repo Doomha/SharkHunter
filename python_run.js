1c77a432856e7d4182729045aefa4ae9932055da
Doomha <nathaniel.hart@gmail.com>
2022-01-26 17:56:12 -0700

    browserify and gen bug fixes

--- python_run.js
+++ python_run.js
@@ -1,12 +1,21 @@
-const {PythonShell} = require ('python-shell');
 
-let options = {
-  mode: "text",
-  pythonOptions: ["-u"],
-  args: ["m", "https://www.gymshark.com/products/gymshark-apex-seamless-sports-bra-light-blue"]
-}
+const {PythonShell} = require ("python-shell");
+require("dotenv").config()
+let master_pin = process.env.MASTER_PIN;
 
-PythonShell.run("sharkbot.py", options, function (err) {
-  if (err) throw err;
-  console.log("finished");
-});
+
+function bot_call () {
+    let options = {
+      mode: "text",
+      pythonOptions: ["-u"],
+      args: ["m", "https://www.gymshark.com/products/gymshark-apex-seamless-sports-bra-light-blue"]
+    }
+
+    PythonShell.run("sharkbot.py", options, function (err) {
+      if (err) throw err;
+      console.log("finished");
+    });
+  }
+
+
+/* console.log(process.env) */
