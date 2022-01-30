e02aae1c9a20a88e6849c34b2862608eed24306b
Doomha <nathaniel.hart@gmail.com>
2022-01-30 12:28:12 -0700

    Docker fixes & misc bug fixes

--- express_folder/python_run.js
+++ express_folder/python_run.js
@@ -1,34 +1,30 @@
 // require("dotenv").config();
 // function find_pin() {
-//   let master_pin = process.env.MASTER_PIN;
+//   let master_pin = ../process.env.MASTER_PIN;
 //   console.log(master_pin);
 // }
 //
-// function bot_call() {
-//   const {PythonShell} = require ("python-shell");
-//     let options = {
-//       mode: "text",
-//       pythonOptions: ["-u"],
-//       args: ["m", "https://www.gymshark.com/products/gymshark-apex-seamless-sports-bra-light-blue"]
-//     }
-//
-//     PythonShell.run("python/sharkbot.py", options, function (err) {
-//       if (err) throw err;
-//       console.log("finished");
-//     });
-//   }
+const {PythonShell} = require ("python-shell");
+  let options = {
+    mode: "text",
+    pythonOptions: ["-u"],
+    args: ["m", "https://www.gymshark.com/products/gymshark-apex-seamless-sports-bra-light-blue"]
+  }
 
-// import { done_status } from '../../server.js';
-//
+  PythonShell.run("../python/sharkbot.py", options, function (err) {
+    if (err) throw err;
+    console.log("finished");
+  });
+
 // function hello_world() {
 //   console.log("Hello World!")
 // }
-//
-function set_done_status(){
-  done_status = true;
-  console.log("Done status is " + done_status + ".")
-  return done_status;
-}
+
+// function set_done_status(){
+//   done_status = true;
+//   console.log("Done status is " + done_status + ".")
+//   return done_status;
+// }
 
 
 /* console.log(process.env) */
--- server.js
+++ server.js
@@ -5,10 +5,10 @@ require("dotenv").config();
 
 const app = express();
 app.use(express.json());
-app.use(express.static("express_folder"));
+app.use(express.static('express_folder'));
 
 app.use('/', function(req,res){
-    res.sendFile(path.join(__dirname+'/SharkHunter/express_folder'));
+    res.sendFile(path.join(__dirname+'express_folder'));
   });
 
 
@@ -25,26 +25,27 @@ console.debug('Server listening on port ' + port);
 // }
 // find_pin();
 
-let done_status = false;
-const done_yet_check = new Promise((resolve, reject) => {
-  if (done_status){
-    const done_yet = "All finished."
-    resolve(done_yet)
-  } else {
-    const not_done_yet = "Still working on that."
-    reject(not_done_yet)
-  }
-})
-
-const work_check = () => {
-  done_yet_check
-    .then(ok => {
-      console.log(ok)
-    })
-    .catch(err => {
-      console.error(err)
-    })
-}
-
-work_check();
+// let done_status = false;
+// const done_yet_check = new Promise((resolve, reject) => {
+//   if (done_status){
+//     const done_yet = "All finished."
+//     resolve(done_yet)
+//   } else {
+//     const not_done_yet = "Still working on that."
+//     reject(not_done_yet)
+//   }
+// })
+//
+// const work_check = () => {
+//   done_yet_check
+//     .then(ok => {
+//       console.log(ok)
+//     })
+//     .catch(err => {
+//       console.error(err)
+//     })
+// }
+//
+// work_check();
 done_status = true;
+console.log(done_status);
